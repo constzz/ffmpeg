c6e7e6f22199cb448534fc092fffd6f72e801bfd
Konstantin Bezzemelnyi <77053754+constzz@users.noreply.github.com>
2025-04-18 13:32:12 +0300

    Add

---
+++ Package.swift
@@ -0,0 +1,43 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+let package = Package(
+    name: "ffmpeg-kit",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "ffmpeg-kit",
+            targets: ["ffmpeg-kit"]
+        ),
+    ],
+    dependencies: [
+        // Add external dependencies here if needed
+    ],
+    targets: [
+        .target(
+            name: "ffmpeg-kit",
+            dependencies: [],
+            path: "apple/src",
+            exclude: ["README.md"],
+            publicHeadersPath: ".",
+            cSettings: [
+                .headerSearchPath("apple/src/"), // Add path to FFmpeg headers
+                .headerSearchPath("apple/src/FFmpegKitConfig"), // Add path to FFmpeg headers
+                .define("FFMPEG_KIT", to: "1")
+            ],
+            linkerSettings: [
+                .linkedLibrary("avutil"),
+                .linkedLibrary("avcodec"),
+                .linkedLibrary("avformat"), // Add other required FFmpeg libraries
+            ]
+        ),
+        .testTarget(
+            name: "ffmpeg-kitTests",
+            dependencies: ["ffmpeg-kit"],
+            path: "Tests"
+        ),
+    ]
+)
